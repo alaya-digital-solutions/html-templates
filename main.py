406785b4996cfc44d3292f82fde449ecb7716ff5
matheus695p <mateu_695p@hotmail.com>
2021-03-05 18:40:46 -0300

    add: permitir agregar imagenes

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ from flask import (Flask, redirect, urlfor, render_template)
 
 app = Flask(__name__)
 
-
 @app.route("/home")
 def home():
-    return render_template("index.html", content="matheus")
-
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()

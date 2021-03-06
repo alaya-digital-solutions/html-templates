1b51d411d23b2b98f2d3339d1fc9db50475f5c35
matheus695p <mateu_695p@hotmail.com>
2021-03-06 09:11:27 -0300

    add: testeando codigo python dentro del html

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from flask import (Flask, redirect, urlfor, render_template)
+from flask import (Flask, render_template)
 
 app = Flask(__name__)
 
-@app.route("/home")
+
+@app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", content=[["CAEX 32", "182388", "125"],
+                                                  ["CAEX 64", "182334", "185"],
+                                                  ["CAEX 21", "182334", "195"],
+                                                  ["CAEX 41", "182334", "269"]])
+
 
 if __name__ == "__main__":
     app.run()
+
+
+[["CAEX 32", "182388", "125"],
+ ["CAEX 64", "182334", "185"],
+ ["CAEX 21", "182334", "195"],
+ ["CAEX 41", "182334", "269"]]
+
+
+lista = [["CAEX 32", "182388", "125"], ["CAEX 64", "182334", "185"],
+         ["CAEX 21", "182334", "195"],
+         ["CAEX 41", "182334", "269"]]
+for i, j, k in range(len(lista)):
+    print(i, j, k)

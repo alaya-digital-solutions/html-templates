5d6c67dd52fa07151b71290e3349d55c9e374f04
matheus695p <mateu_695p@hotmail.com>
2021-03-06 19:54:20 -0300

    fix: vamo a ver

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
+import pandas as pd
 from flask import (Flask, render_template)
 
-app = Flask(__name__)
+# lanzar la aplicación web
+app = Flask(__name__, template_folder='templates')
 
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html", content=[["CAEX 32", "182388", "125"],
-                                                  ["CAEX 64", "182334", "185"],
-                                                  ["CAEX 21", "182334", "195"],
-                                                  ["CAEX 41", "182334", "269"]])
+    test = pd.read_csv('data/test.csv')
+    equipo = test['equipo']
+    id_neumatico = test['id_neumatico']
+    remaining_useful_life = test['remanent_useful_life']
+    print(equipo, id_neumatico, remaining_useful_life)
+    return render_template('index.html',
+                           tables={
+                               'equipo': equipo,
+                               'id_neumatico': id_neumatico,
+                               'remaining_useful_life': remaining_useful_life})
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    app.debug = True
     app.run()
 
 
-[["CAEX 32", "182388", "125"],
- ["CAEX 64", "182334", "185"],
- ["CAEX 21", "182334", "195"],
- ["CAEX 41", "182334", "269"]]
+content = [["CAEX 32", "182388", "10"], ["CAEX 64", "182334", "185"],
+           ["CAEX 21", "382334", "52"],
+           ["CAEX 41", "482334", "64"],
+           ["CAEX 43", "582344", "124"],
+           ["CAEX 69", "682367", "158"],
+           ["CAEX 74", "882374", "269"]]
 
+content = pd.DataFrame(content, columns=["equipo", "id_neumatico",
+                                         "remanent_useful_life"])
+test = content.copy()
+equipo = test['equipo']
+id_neumatico = test['id_neumatico']
+remaining_useful_life = test['remanent_useful_life']
 
-lista = [["CAEX 32", "182388", "125"], ["CAEX 64", "182334", "185"],
-         ["CAEX 21", "182334", "195"],
-         ["CAEX 41", "182334", "269"]]
-for i, j, k in range(len(lista)):
-    print(i, j, k)
+tables = {
+    'equipo': equipo,
+    'id_neumatico': id_neumatico,
+    'remaining_useful_life': remaining_useful_life
+}
+
+content.to_csv("data/test.csv", index=False)
